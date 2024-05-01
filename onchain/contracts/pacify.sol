9ee6957cc8f516293e3107840614193eff3d8846
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-05-01 14:46:27 +0300

    feat: Contract dev't

---
+++ onchain/contracts/pacify.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract Pacify {
+    
+}
