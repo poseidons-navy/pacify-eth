1cb3a8dda2adf163222d69f953b78aab43c10200
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-05-02 22:35:31 +0300

    feat: mint nft function

--- onchain/contracts/pacify.sol
+++ onchain/contracts/pacify.sol
@@ -1,10 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "hardhat/console.sol";
 
-contract Pacify is ERC721URIStorage{
-    
+contract Pacific is ERC721URIStorage {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("Pacify", "Pacific") {}
+
+    function mintCert(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _mint(recipient, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
 }
