202deb5183d727a74ba831e45b1115cd0e9ab2c4
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-05-02 11:53:52 +0300

    feat: Initialize frontend

--- onchain/contracts/pacify.sol
+++ onchain/contracts/pacify.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "hardhat/console.sol";
 
-contract Pacify {
+contract Pacify is ERC721URIStorage{
     
 }
